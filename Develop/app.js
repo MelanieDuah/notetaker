be6ebacfa654e5265c5214255897ed5a48a88350
melanie <melanieduah@gmail.com>
2020-06-24 00:35:31 -0500

    Changed port node to take from env.PORT

--- Develop/app.js
+++ Develop/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const port = 80;
+const port = process.env.PORT || 3000;
 const fs = require('fs');
 const filesPath = path.join(__dirname, 'public');
 
