3ae3ef9a78f9debdc5187295bf4e129c2a2a84b9
melanie <melanieduah@gmail.com>
2020-06-24 00:31:51 -0500

    Change listening port to 80

--- Develop/app.js
+++ Develop/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 80;
 const fs = require('fs');
 const filesPath = path.join(__dirname, 'public');
 
